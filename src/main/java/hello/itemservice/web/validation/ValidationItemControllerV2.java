93757301fb22e7afbc48e4d7ed59c1519eef0800
coollpice <coollpice@naver.com>
2021-08-29 20:25:30 +0900

    v2

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -43,22 +43,22 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
-    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult , RedirectAttributes redirectAttributes , Model model) {
+    //    @PostMapping("/add")
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         // model 에 담기는 객체명 , 객체의 필드명 , 메세지내용
-        
+
         //검증시작
         if (!StringUtils.hasText(item.getItemName())) {
-            bindingResult.addError(new FieldError("item" , "itemName" , "상품명은 필수입니다."));
+            bindingResult.addError(new FieldError("item", "itemName", "상품명은 필수입니다."));
         }
 
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            bindingResult.addError(new FieldError("item" , "price" , "가격은 1,000 ~ 1,000,000 까지 허용됩니다."));
+            bindingResult.addError(new FieldError("item", "price", "가격은 1,000 ~ 1,000,000 까지 허용됩니다."));
         }
 
         if (item.getQuantity() == null || item.getQuantity() > 9999) {
-            bindingResult.addError(new FieldError("item" , "quantity" , "수량은 최대 9,999개 까지만 허용됩니다."));
+            bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999개 까지만 허용됩니다."));
         }
 
 
@@ -68,7 +68,7 @@ public class ValidationItemControllerV2 {
 
             if (resultPrice < 10000) {
                 // 필드에관한 에러가 아니기 때문에 , ObjectError 사용
-                bindingResult.addError(new ObjectError("item" , "가격 * 수량의 합은 10,000원 이상이여야 합니다. 현재 값 : " + resultPrice));
+                bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이여야 합니다. 현재 값 : " + resultPrice));
             }
         }
 
@@ -84,6 +84,50 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
+
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        // model 에 담기는 객체명 , 객체의 필드명 , form에잘못 입력된 내용 , 바인딩여부,  ,   ,   메세지내용
+
+        //검증시작
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품명은 필수입니다."));
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용됩니다."));
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null , "수량은 최대 9,999개 까지만 허용됩니다."));
+        }
+
+
+        // 글로벌 에러처리
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+
+            if (resultPrice < 10000) {
+                // 필드에관한 에러가 아니기 때문에 , ObjectError 사용
+                bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이여야 합니다. 현재 값 : " + resultPrice));
+            }
+        }
+
+        //검증 실패시 다시 입력 폼으로
+
+        if (bindingResult.hasErrors()) {
+            return "validation/v2/addForm";
+        }
+
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
